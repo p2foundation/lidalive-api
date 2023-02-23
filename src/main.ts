48e320094863a83d197d026baf8e7a32a9ece0db
p2foundation <p2found@gmail.com>
2023-02-23 15:33:25 +0000

    prep codepipeline

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { SERVER_PORT } from './constants';
+
 import { FallbackExceptionFilter } from './filters/fallback.filter';
 import { HttpExceptionFilter } from './filters/http.filter';
 import { ValidationException } from './filters/validation.exception';
-
 
 async function bootstrap() {
   const logger = new Logger(bootstrap.name);
@@ -15,7 +15,6 @@ async function bootstrap() {
   app.use(helmet());
 
   app.useGlobalPipes(new ValidationPipe());
-
 
   const port = process.env.PORT || SERVER_PORT;
   await app.listen(port);
